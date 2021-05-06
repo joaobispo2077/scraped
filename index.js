8a83f270444dcd10dca8a0f66136d6297a16a2cf
João Bispo <joaobispo2077@gmail.com>
2021-05-06 16:21:50 -0300

    chore: send web scraping to tmp folder

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://twitter.com/');
-  await page.screenshot({ path: 'twitter.png' });
+  await page.screenshot({ path: path.join(__dirname, 'tmp', 'twitter.png') });
 
   await browser.close();
 })();
