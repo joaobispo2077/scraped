f2726edbaee6217ec0501cc1b9d7a1219c4d974f
João Bispo <joaobispo2077@gmail.com>
2021-05-06 17:17:59 -0300

    chore: trying scraping insta nerdstore

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://twitter.com/');
-  await page.screenshot({ path: path.join(__dirname, 'tmp', 'twitter.png') });
+  await page.goto('https://www.instagram.com/nerdstore/');
+
+  await page.screenshot({ path: path.join(__dirname, 'tmp', 'nerdstore.png') });
+  const images = await page.evaluate(() => {
+    // selecionar postagens
+    const nodeList = document.querySelectorAll('article img');
+    // transformar node list em array
+    const posts = [...nodeList];
+
+    // transformar os elementos html em objetos json
+    const images = posts.map(post => ({
+      src: post.src
+    }));
+
+    // retornar
+    console.log(images);
+    return images;
+  });
+
+  fs.writeFile('tmp/nerdstore.json', JSON.stringify(images, null, 2), err => {
+    if (err) throw new Error('Something went wrong');
+
+    console.log('well done!');
+  });
 
   await browser.close();
 })();
